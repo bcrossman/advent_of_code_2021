18f2edb3fe66519a467dbe53c16bea6f0687d31d
Brent <bcrossman@oldportconsulting.com>
2021-12-03 16:08:02 -0500

    Update Day_3_Best_of_twitter.R

--- Day_3_Best_of_twitter.R
+++ Day_3_Best_of_twitter.R
@@ -3,18 +3,16 @@
 
 input <- read.fwf("./Day_3/Part_1/input.txt", widths = c(rep(1, 12)))
 
+## Part 1
 gamma <- apply(input, 2, function(x) ceiling(median(x)))
 epsilon <- as.integer(sapply(gamma, `!`))  #0/1 works kind of like false / true, so the ! reverses a 0 or a 1
 
 gamma_dec <- strtoi(paste0(gamma, collapse = ''), 2)  #originally I learned this str_toi from @_Riinu_, so neat
 epsilon_dec <- strtoi(paste0(epsilon, collapse = ''), 2)
 
-
 gamma_dec * epsilon_dec
 
-
-# Part 2
-
+## Part 2
 oxy <- input
 co2 <- input
 oxy$dec <- apply(oxy, 1, function(x) strtoi(paste0(x, collapse = ''), 2))
@@ -23,7 +21,6 @@ co2$dec <- apply(co2, 1, function(x) strtoi(paste0(x, collapse = ''), 2))
 while (nrow(oxy) > 1) {
   oxy <- oxy[oxy[[1]]==ceiling(median(oxy[[1]])), -1, drop = F]
 }
-
 
 while (nrow(co2) > 1) {
   co2 <- co2[co2[[1]]==as.integer(sapply(ceiling(median(co2[[1]])), `!`)), -1, drop = F]
@@ -47,7 +44,7 @@ binary_to_int <- function(x) {
 common <-most_common(input)
 binary_to_int(common) * binary_to_int(!common)
 
-#part 2
+# Part 2
 
 power_consumption(input)
 
@@ -60,7 +57,7 @@ for (j in 1:ncol(input)) {
   }
   if (nrow(co2) > 1) {
     common <- most_common(co2)
-    co2 <- co2[co2[, j] != common[j], ]
+    co2 <- co2[co2[, j] != common[j], ]  #uses that ! to inverse the 0/1 again.
   }
 }
 binary_to_int(oxygen) * binary_to_int(co2)
