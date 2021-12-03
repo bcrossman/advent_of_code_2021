c2eafe11ef9c528a18eb8ce68941eb0dab84b4c4
Brent <bcrossman@oldportconsulting.com>
2021-12-03 16:19:47 -0500

    Update Day_3_Best_of_twitter.R

--- Day_3_Best_of_twitter.R
+++ Day_3_Best_of_twitter.R
@@ -30,6 +30,8 @@ oxy$dec * co2$dec
 
 ## Cool one from @TeaStats
 
+input <- read.fwf("./Day_3/Part_1/input.txt", widths = c(rep(1, 12)))
+
 # functions
 
 most_common <- function(x) {
@@ -61,3 +63,25 @@ for (j in 1:ncol(input)) {
   }
 }
 binary_to_int(oxygen) * binary_to_int(co2)
+
+
+## and one more from @antoine_fabri
+
+input <- scan("./Day_3/Part_1/input.txt", what = character(), sep = "", quiet = TRUE)
+mat   <- do.call(rbind, strsplit(input, "")) == 1  ## makes 0 /1 true/false
+
+# part 1
+gamma <- colMeans(mat) >= .5
+epsilon <- !gamma
+powers <- 2^(rev(seq_along(gamma)-1))
+sum(powers * gamma) * sum(powers * epsilon)
+
+# part 2
+oxygen <- co2 <- mat
+for (i in seq(ncol(mat))) {
+  if (is.matrix(oxygen))
+    oxygen <- oxygen[oxygen[,i] == (mean(oxygen[,i]) >= .5),]
+  if (is.matrix(co2))
+    co2 <- co2[co2[,i] == (mean(co2[,i]) < .5),]
+}
+sum(powers * oxygen) * sum(powers * co2)
